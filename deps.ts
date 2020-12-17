6a20cf67adc14182b89e5820f7f2495e95e0d589
Kaan <kaaninel@gmail.com>
2020-12-17 12:41:04 +0300

    deno fmt

--- deps.ts
+++ deps.ts
@@ -12,9 +12,7 @@ export {
   Syntax,
 } from "https://deno.land/x/protoc_parser@v0.2.7/mod.ts";
 
-export type {
-  Visitor
-} from "https://deno.land/x/protoc_parser@v0.2.7/mod.ts";
+export type { Visitor } from "https://deno.land/x/protoc_parser@v0.2.7/mod.ts";
 
 export { decode, encode } from "https://deno.land/x/varint@v2.0.0/varint.ts";
 
