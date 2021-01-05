4d00a3fa5bc45701dc73e24405a6ac67fbaf265a
Kaan İnel <kaaninel@gmail.com>
2021-01-05 21:14:46 +0300

    fix: export type for Visitor (#6)
    * Export type for Visitor
    
    * deno fmt
    
    * chore: upgrade deps to latest
    
    Co-authored-by: Keith Cirkel <keithamus@users.noreply.github.com>

--- deps.ts
+++ deps.ts
@@ -10,9 +10,10 @@ export {
   parse,
   Proto,
   Syntax,
-  Visitor,
-} from "https://deno.land/x/protoc_parser@v0.2.7/mod.ts";
+} from "https://deno.land/x/protoc_parser@v0.2.8/mod.ts";
+
+export type { Visitor } from "https://deno.land/x/protoc_parser@v0.2.8/mod.ts";
 
 export { decode, encode } from "https://deno.land/x/varint@v2.0.0/varint.ts";
 
-export { dirname, join } from "https://deno.land/x/std@0.67.0/path/mod.ts";
+export { dirname, join } from "https://deno.land/x/std@0.83.0/path/mod.ts";
