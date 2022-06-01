6c884716254e4b94bc98bef43ddaac813d26fdb6
Alexandre Blause <46499306+ablause@users.noreply.github.com>
2022-06-01 17:32:05 +0200

    chore: update deps (#9)

--- deps.ts
+++ deps.ts
@@ -10,10 +10,10 @@ export {
   parse,
   Proto,
   Syntax,
-} from "https://deno.land/x/protoc_parser@v0.2.8/mod.ts";
+} from "https://deno.land/x/protoc_parser@v0.2.9/mod.ts";
 
-export type { Visitor } from "https://deno.land/x/protoc_parser@v0.2.8/mod.ts";
+export type { Visitor } from "https://deno.land/x/protoc_parser@v0.2.9/mod.ts";
 
 export { decode, encode } from "https://deno.land/x/varint@v2.0.0/varint.ts";
 
-export { dirname, join } from "https://deno.land/x/std@0.133.0/path/mod.ts";
+export { dirname, join } from "https://deno.land/x/std@0.141.0/path/mod.ts";
